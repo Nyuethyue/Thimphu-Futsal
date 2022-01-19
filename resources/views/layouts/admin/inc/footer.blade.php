c25ebe579e8696b05e73b6c148df21447486b01d
dawa.tshering <dawa.tshering@thimphutechpark.bt>
2022-01-19 16:06:05 +0600

    frontend integrated

---
+++ resources/views/layouts/admin/inc/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      @nyuethyue
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022 <a class="text-decoration-none" href="https://adminlte.io">thimphu futsal</a>.</strong> All rights reserved.
+  </footer>
--- resources/views/layouts/inc/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      @nyuethyue
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2022 <a class="text-decoration-none" href="https://adminlte.io">thimphu futsal</a>.</strong> All rights reserved.
-  </footer>
